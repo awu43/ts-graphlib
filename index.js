1de8071d3e80221541541e30bc7983052484b1b1
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-07-22 21:47:53 -0700

    Delete version

--- index.js
+++ index.js
@@ -34,5 +34,4 @@ module.exports = {
   Graph: lib.Graph,
   json: require("./lib/json"),
   alg: require("./lib/alg"),
-  version: lib.version
 };
--- test/version.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require("chai");
-
-describe("version", function () {
-  it("should match the version from package.json", function () {
-    const packageVersion = require("../package.json").version;
-    expect(require("../lib/version")).to.equal(packageVersion);
-  });
-});
