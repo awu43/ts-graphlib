1fa7263d821c1ce97d850df6bbb84550dff19dc9
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-07-24 16:40:17 -0700

    Add missing docs

--- src/alg/find-cycles.ts
+++ src/alg/find-cycles.ts
@@ -1,6 +1,17 @@
 import tarjan from "./tarjan";
 import type Graph from "../graph";
 
+/**
+ * Given a Graph, graph, this function returns all nodes that are part of a cycle. As there
+ * may be more than one cycle in a graph this function return an array of these cycles,
+ * where each cycle is itself represented by an array of ids for each node involved in
+ * that cycle. Method alg.isAcyclic is more efficient if you only need to determine whether a graph has a
+ * cycle or not.
+ * Complexity: O(|V| + |E|).
+ *
+ * @argument graph - graph where to search cycles.
+ * @returns cycles list.
+ */
 export default function findCycles(g: Graph): string[][] {
   return tarjan(g).filter(
     cmpt =>
--- src/alg/is-acyclic.ts
+++ src/alg/is-acyclic.ts
@@ -2,6 +2,14 @@ import type Graph from "../graph";
 
 import topsort from "./topsort";
 
+/**
+ * Given a Graph, graph, this function returns true if the graph has no cycles and returns false if it
+ * does. This algorithm returns as soon as it detects the first cycle. You can use alg.findCycles
+ * to get the actual list of cycles in the graph.
+ *
+ * @argument graph - graph to detect whether it acyclic ot not.
+ * @returns whether graph contain cycles or not.
+ */
 export default function isAcyclic(g: Graph): boolean {
   try {
     topsort(g);
