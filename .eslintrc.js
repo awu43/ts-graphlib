b7e969998edb8913a946361baa0478713115e3cb
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-07-26 23:41:22 -0700

    Refactor ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2020: true,
-    node: true,
-    mocha: true,
-  },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  rules: {
-    "func-names": 0,
-    "no-underscore-dangle": 0,
-    "no-else-return": 0,
-    "consistent-return": 0,
-    "no-param-reassign": [2, { props: false }],
-    "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
-    "no-restricted-syntax": [
-      2,
-      "ForInStatement",
-      "LabeledStatement",
-      "WithStatement",
-    ],
-    "max-classes-per-file": 0,
-
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-  },
+  extends: ["./.base-eslint-config.js"],
   overrides: [
     {
       files: ["src/**/*.ts"],
       extends: [
-        "airbnb-base",
-        "plugin:prettier/recommended",
+        "./.base-eslint-config.js",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
       ],
       parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint", "prettier"],
+      plugins: ["@typescript-eslint"],
       rules: {
-        "func-names": 0,
-        "no-underscore-dangle": 0,
-        "no-else-return": 0,
-        "consistent-return": 0,
-        "no-param-reassign": [2, { props: false }],
-        "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
-        "no-restricted-syntax": [
-          2,
-          "ForInStatement",
-          "LabeledStatement",
-          "WithStatement",
-        ],
-        "max-classes-per-file": 0,
-
-        "import/no-unresolved": 0,
-        "import/extensions": 0,
         "lines-between-class-members": [
           2,
           "always",
@@ -66,11 +20,8 @@ module.exports = {
     },
     {
       files: ["test/**/*.test.js"],
-      extends: ["airbnb-base", "plugin:prettier/recommended"],
+      extends: ["./.base-eslint-config.js"],
       rules: {
-        "import/no-unresolved": 0,
-        "import/extensions": 0,
-
         "no-unused-expressions": 0,
         "global-require": 0,
       },
